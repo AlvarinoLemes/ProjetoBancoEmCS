a003dcf271eda1a3525f442f9cef223ef553075f
Alvarino Lemes <63159901+AlvarinoLemes@users.noreply.github.com>
2022-09-07 17:18:24 -0300

    Add files via upload
    Primeiro commit

---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region Codigo
+using Banco;
+
+//Dica
+List<Clente> Clientes = new List<Clente>();
+ConsultarCliente();
+
+                  // var codigo = Console.ReadLine();
+//Se o código do cliente não existir, perguntar se deseja fazer o cadastro
+void ConsultarCliente()
+{
+    Console.WriteLine("Bem-vindo ao seu banco!");
+    Console.WriteLine("Digite o código da sua conta:");
+    string codigo = Console.ReadLine();
+    Cliente cliente = null;
+}
